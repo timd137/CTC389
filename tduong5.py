e2fa48474e74ec2db0c6a79c842ab81ba9be35ef
timd137 <tduong47@toromail.csudh.edu>
2026-08-20 01:41:09 +0000

    lab #5

---
+++ tduong5.py
@@ -0,0 +1,25 @@
+#Timothy Duong
+#CTC 389 Lab 5
+
+even_nums = [32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384]
+
+print("No Loop")
+print(even_nums[0])
+print(even_nums[1])
+print(even_nums[2])
+print(even_nums[3])
+print(even_nums[4])
+print(even_nums[5])
+print(even_nums[6])
+print(even_nums[7])
+print(even_nums[8])
+print(even_nums[9])
+
+print("With Loop")
+for num in even_nums:
+    print(num)
+
+fruits = ["banana", "strawberry", "watermelon", "lemon", "pineapple"]
+
+for fruit in fruits:
+    print(fruit)
