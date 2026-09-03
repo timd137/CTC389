7528f39b73c923c0de3326ad7bb993c442244cbf
timd137 <tduong47@toromail.csudh.edu>
2026-09-03 02:54:29 +0000

    tduong7_2.py

---
+++ tduong7_2.py
@@ -0,0 +1,22 @@
+#Timothy Duong
+#CTC 389 Lab 7 Part 2
+
+number = 3
+
+def guessing_game():
+    guess = int(input("Guess my number: "))
+
+    while guess >= number-2 and guess <= number+2 and guess != number:
+        guess = int(input("Close, try again: "))
+    if guess == number:
+        print("Well done! You guessed my number")
+    elif guess > number+2:
+        print("Sorry, you lost. Your guess was higher than my number which is", number)
+    elif guess < number-2:
+        print("Sorry, you lost. Your guess was lower than my number which is", number)
+
+response = input("Would you like to play my game? ")
+if response == "yes":
+    while response != "no":
+        guessing_game()
+        response = input("Would you like to play my game? ")
