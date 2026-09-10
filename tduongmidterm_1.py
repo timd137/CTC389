3e6a155584692328246f2cd58387c857cfa7a703
timd137 <tduong47@toromail.csudh.edu>
2026-09-10 00:14:56 +0000

    midterm question 1

---
+++ tduongmidterm_1.py
@@ -0,0 +1,9 @@
+#Timothy Duong
+#CTC 389 Midterm Question 1
+
+name = input("Enter your name: ")
+number = int(input("Enter a number: "))
+number_to_add = int(input("How much to add to that number? "))
+final_number = number + number_to_add
+
+print(name, "you entered", number, "and added", number_to_add, "to get a total of", final_number)
